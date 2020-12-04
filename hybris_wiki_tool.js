74fb8d60f835761c789f09e8ed8d8a20ed60a129
tonyjiangde <tao.jiang02@sap.com>
2020-12-04 10:22:26 +0800

    Create hybris_wiki_tool.js

---
+++ hybris_wiki_tool.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name        Hybris Wiki Tools
+// @namespace   I314119
+// @match       https://cxwiki.sap.com/*
+// @version     0.0.1
+// @author      I314119
+// @description Wiki tool
+// 
+// @grant       GM_getValue
+// @grant       GM_setValue
+// @grant       GM_deleteValue
+// @grant       GM_listValues
+// 
+// @history     0.0.1   Release: 04.12.2020
+// ==/UserScript==
+
+console.log("test");
